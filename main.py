3bde5aae7536b1a22c6e8ff2c9f47fa01a00c73e
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2017-01-30 00:04:30 -0600

    Created web application for encrypting input

--- main.py
+++ main.py
@@ -16,12 +16,65 @@
 #
 import webapp2
 import caesar
+import cgi
+
+form = """
+    <form method="post">
+        <textarea name="message" value="%(encrypt_text)s">
+        <br>
+        <label> Amount to Rotate
+            <input type="text" name="rotate_amount" value="%(rotate_amount)s">
+        </label>
+        <input type="submit">
+    </form>
+"""
+
+def build_page(message=""):
+        header = "<h2>Web Caesar</h2>"
+
+        rot_label = "<label>Rotate by: </label>"
+        rotation_input = "<input type='number' name='rotation'/>"
+
+        message_label = "<label>Message: </label>"
+        textarea = "<textarea name='message'>" + message + "</textarea>"
+
+        submit = "<input type='submit'>"
+
+        form = ("<form method='post'>" +
+                rot_label + rotation_input + "<br>" +
+                message_label + textarea + "<br>" +
+                submit + "</form>")
+
+        return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hello, world!"
-        encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+
+        self.response.write(build_page())
+
+    def post(self):
+        message = self.request.get("message")
+
+        rotation_text = self.request.get("rotation")
+        if len(rotation_text) > 0:
+            rotation = int(rotation_text)
+        else:
+            rotation = 0
+
+        encrypted_message = caesar.encrypt(message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        self.response.write(build_page(escaped_message))
+
+
+    # def write_form(self, error="", encrypt_text=""):
+    #     self.response.out.write(form % {"error": error,
+    #                                     "month": month,
+    #                                     "day": day,
+    #                                     "year": year})
+    #
+    # def get(self):
+    #     #self.response.write(form)
+    #     self.write_form()
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
